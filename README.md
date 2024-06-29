3a6618eceb006947e0f1568f5415811ca8a94e98
osag1e <osagie6862@gmail.com>
2024-06-29 03:04:23 +0100

    feat: updated config

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # my-neovim-config
-A Neovim configuration that I enjoy using with tmux, which currently includes Go and C LSPs.
+A Neovim configuration that I enjoy using with tmux. 
 
-![my-neovim-config](https://github.com/osag1e/my-neovim-config/blob/main/nvim.png)
+![my-neovim-config](https://github.com/osag1e/my-neovim-config/blob/main/vim.png)
 
 
 
--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("osagie.core.options")
 
+
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
@@ -10,14 +11,21 @@ require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' 
   use 'nvim-treesitter/nvim-treesitter' 
   use 'neovim/nvim-lspconfig' 
-  use 'hrsh7th/nvim-compe' 
+  use 'gruvbox-community/gruvbox'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp' 
+  use 'hrsh7th/cmp-buffer' 
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline' 
+  use 'L3MON4D3/LuaSnip' 
+  use 'saadparwaiz1/cmp_luasnip' 
 end)
 
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"go", "c"}, 
+  ensure_installed = {"go", "c", "rust", "javascript"},
   highlight = {
-    enable = true,              
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
 }
@@ -28,7 +36,6 @@ local lspconfig = require'lspconfig'
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local opts = { noremap=true, silent=true }
-
 
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -41,39 +48,13 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>', opts)
-
-  require'compe'.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'enable';
-    throttle_time = 80;
-    source_timeout = 200;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = true;
-
-    source = {
-      path = true;
-      buffer = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-    };
-  }
-
-  buf_set_keymap('i', '<C-Space>', 'compe#complete()', { noremap = true, silent = true, expr = true })
-  buf_set_keymap('i', '<CR>', 'compe#confirm("<CR>")', { noremap = true, silent = true, expr = true })
-  buf_set_keymap('i', '<C-e>', 'compe#close("<C-e>")', { noremap = true, silent = true, expr = true })
 end
 
 lspconfig.gopls.setup{
   on_attach = on_attach,
   settings = {
     gopls = {
-      gofumpt = true, 
+      gofumpt = true,
     },
   },
 }
@@ -82,4 +63,64 @@ lspconfig.clangd.setup{
   on_attach = on_attach,
 }
 
+lspconfig.rust_analyzer.setup{}
+
+lspconfig.tsserver.setup{
+  on_attach = on_attach,
+}
+
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+})
+
 vim.o.completeopt = 'menuone,noselect'
+
+vim.o.background = "dark"
+vim.g.gruvbox_contrast_dark = "hard"
+
+vim.cmd [[colorscheme gruvbox]]
+
+vim.cmd [[highlight Comment guifg=#b8bb26]]
