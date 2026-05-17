14ed94fd6c9e1a9b337303457b4ebaea2b3dc30a
OsagieDG <osagie6862@gmail.com>
2026-05-17 17:12:14 +0100

    removed treesitter...

--- lua/osagie/plugins/init.lua
+++ lua/osagie/plugins/init.lua
@@ -1,5 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if vim.fn.isdirectory(lazypath) == 0 then
+
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -12,51 +13,69 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+-- Plugins
 require("lazy").setup({
+  -- Treesitter
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     lazy = false,
-    build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter').install({
-        "c", "lua",
-        "go", "odin",
-        "swift", "kotlin",
-        "javascript", "python",
-        "svelte", "rust",
-        "html", "css",
-        "yaml",
+      require("nvim-treesitter").setup({
+        ensure_installed = {
+          "c", "lua",
+          "go", "odin",
+          "swift", "kotlin",
+          "javascript", "python",
+          "svelte", "rust",
+          "html", "css",
+          "yaml",
+        },
+
+        highlight = {
+          enable = true,
+        },
+
+        auto_install = true,
       })
     end,
   },
-  { 'neovim/nvim-lspconfig' },
-  { 'hrsh7th/nvim-cmp' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-  { 'hrsh7th/cmp-buffer' },
-  { 'hrsh7th/cmp-path' },
-  { 'hrsh7th/cmp-cmdline' },
-  { 'L3MON4D3/LuaSnip' },
-  { 'saadparwaiz1/cmp_luasnip' },
 
+  -- LSP
+  { "neovim/nvim-lspconfig" },
+
+  -- Completion
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+
+  -- Snippets
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+
+  -- Telescope
   {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      -- Load telescope extensions here
-      require('telescope').load_extension('fzf')
+      require("telescope").setup({})
     end,
   },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
-  },
 
   {
-    'glepnir/lspsaga.nvim',
-    branch = 'main',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+
+  -- LSP UI
+  {
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require('lspsaga').setup({
+      require("lspsaga").setup({
         ui = {
           hover = { border = "rounded" },
         },
@@ -69,15 +88,18 @@ require("lazy").setup({
     end,
   },
 
+  -- Theme
   {
-    'gruvbox-community/gruvbox',
+    "gruvbox-community/gruvbox",
   },
 
+  -- Swift
   {
     "keith/swift.vim",
     ft = "swift",
   },
 
+  -- Xcode tools
   {
     "wojciech-kulik/xcodebuild.nvim",
     dependencies = {
@@ -89,6 +111,7 @@ require("lazy").setup({
   },
 })
 
+-- Diagnostics
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
