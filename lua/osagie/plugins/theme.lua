cdc9d5a1812455440b5b2c8258477daba0b4753a
OsagieDG <osagie6862@gmail.com>
2026-05-19 15:02:26 +0100

    added transparent background

--- lua/osagie/plugins/theme.lua
+++ lua/osagie/plugins/theme.lua
@@ -2,3 +2,7 @@ vim.g.gruvbox_contrast_dark = "hard"
 vim.cmd("colorscheme gruvbox")
 vim.cmd("highlight Comment guifg=#b8bb26")
 
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
+vim.cmd("highlight SignColumn guibg=NONE ctermbg=NONE")
+vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
